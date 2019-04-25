004e9991e86215a69935b859250a741b7d11b883
aleksanderkakol <olcio45@gmail.com>
2019-04-25 15:43:41 +0200

    yield in login.js

--- login.js
+++ login.js
@@ -1,3 +1,13 @@
+let count = yield I.executeAsyncScript(function(done) {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  document.body.appendChild(s);
+  s.src = 'https://yastatic.net/jquery/3.1.1/jquery.min.js';
+  s.onload(function() {
+    alert(typeof($));
+    done();
+  });
+});
 const login_form = document.querySelector('.login_form');
 login_form.addEventListener('submit', function(e) {
   e.preventDefault();
