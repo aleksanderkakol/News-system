5d3c2ebb876406d0ef0bd09b99c8fa0cf38a9a05
aleksanderkakol <olcio45@gmail.com>
2019-05-17 16:29:32 +0200

    edit posts in table

--- menu.js
+++ menu.js
@@ -13,34 +13,29 @@ window.onload = function() {
       if (status === 'success' && data.length > 0) {
         let keys = Object.keys(data[0]);
         for (let i = 0; i < keys.length; i++) {
+
           thead.append('<th>' + keys[i] + '</th>');
         }
-        thead.append('<th>Usuń</th> ');
-        thead.append('<th>Edytuj</th> ');
+        thead.append('<th>Opcje</th> ');
         for (const [index, item] of data.entries()) {
-          let tr = $("<tr></tr>");
+
+          let tr = $(`<tr row_id=${item.id} ></tr>`);
           tbody.append(tr);
-          tr.append('<td>' + item.name + '</td> ');
-          tr.append('<td>' + item.description + '</td> ');
-          tr.append('<td>' + item.is_active + '</td> ');
-          tr.append('<td>' + item.created_at + '</td> ');
-          tr.append('<td>' + item.updated_at + '</td> ');
-          tr.append('<td>' + item.author_id + '</td> ');
-          tr.append("<td><button class='btn_del' alt='Usuń'>Usuń</button></td>");
-          tr.append("<td><button class='btn_edit' alt='Edytuj'>Edytuj</button></td>");
-
-          let btn_del = document.querySelectorAll('.btn_del');
-          let btn_edit = document.querySelectorAll('.btn_edit');
-
-          btn_del[index].addEventListener('click', function() {
-            console.log('usuń');
-          })
-
-          btn_edit[index].addEventListener('click', function() {
-            console.log('edytuj');
-          })
+          tr.append(`<td><div col_name='id' class="row_data">${item.id}</div></td>`);
+          tr.append(`<td><div col_name='name' class="row_data">${item.name}</div></td>`);
+          tr.append(`<td><div col_name='description' class="row_data">${item.description}</div></td>`);
+          tr.append(`<td><div col_name='is_active' class="row_data">${item.is_active}</div></td>`);
+          tr.append('<td><div>' + item.created_at + '</div></td> ');
+          tr.append('<td><div>' + item.updated_at + '</div></td> ');
+          tr.append('<td><div>' + item.author + '</div></td> ');
+          tr.append("<td><div class='options'><button class='btn_edit' alt='Edytuj'>Edytuj</button><button class='btn_save' alt='Zapisz'>Zapisz</button><button class='btn_delete' alt='Usuń'>Usuń</button><button class='btn_cancel' alt='Anuluj'>Anuluj</button></div></td>");
         }
       }
+      $(document).find('.btn_save').hide();
+      $(document).find('.btn_cancel').hide();
+      $(document).find('.btn_delete').hide();
+
+
       if (data['status'] === "false") {
         div.append("<h2 class='error'>" + data.message + "</h2><a class='logout' href='index.html' alt='Login'>Zaloguj ponownie</a>");
         div.css("display", "flex");
@@ -55,6 +50,96 @@ window.onload = function() {
       console.log(status);
     }
   });
+
+
+
+  $(document).on('click', '.btn_edit', function(event) {
+    event.preventDefault();
+    var tbl_row = $(this).closest('tr');
+    var row_id = tbl_row.attr('row_id');
+
+    tbl_row.find('.btn_save').show();
+    tbl_row.find('.btn_cancel').show();
+    tbl_row.find('.btn_delete').show();
+    tbl_row.find('.btn_edit').hide();
+
+    tbl_row.find('.row_data')
+      .attr('contenteditable', 'true')
+      .attr('edit_type', 'button')
+      .addClass('bg-warning')
+      .css('padding', '3px');
+
+
+    tbl_row.find('.row_data').each(function(index, val) {
+      $(this).attr('original_entry', $(this).html());
+    });
+
+  });
+
+  $(document).on('click', '.btn_cancel', function(event) {
+    event.preventDefault();
+    var tbl_row = $(this).closest('tr');
+    var row_id = tbl_row.attr('row_id');
+
+    tbl_row.find('.btn_save').hide();
+    tbl_row.find('.btn_cancel').hide();
+    tbl_row.find('.btn_delete').hide();
+    tbl_row.find('.btn_edit').show();
+
+    tbl_row.find('.row_data')
+      .attr('contenteditable', 'false')
+      .attr('edit_type', 'click')
+      .removeClass('bg-warning')
+
+    tbl_row.find('.row_data').each(function(index, val) {
+      $(this).html($(this).attr('original_entry'));
+    });
+  });
+
+
+  $(document).on('click', '.btn_save', function(event) {
+    event.preventDefault();
+    var tbl_row = $(this).closest('tr');
+
+    tbl_row.find('.btn_save').hide();
+    tbl_row.find('.btn_cancel').hide();
+    tbl_row.find('.btn_delete').hide();
+    tbl_row.find('.btn_edit').show();
+
+    tbl_row.find('.row_data')
+      .attr('contenteditable', 'false')
+      .attr('edit_type', 'click')
+      .removeClass('bg-warning');
+
+    var arr = [{}];
+    tbl_row.find('.row_data').each(function(index, val) {
+      var col_name = $(this).attr('col_name');
+      var col_val = $(this).html();
+      arr[col_name] = col_val;
+    })
+    console.log(arr)
+    for (let i = 0; i < arr.length; i++) {
+      console.log(encodeURIComponent('name[]') + '=' + encodeURIComponent(arr[i]) + '&');
+    }
+    let request = $.ajax({
+      url: "./menu.php",
+      type: "post",
+      data: JSON.stringify(arr)
+    });
+    request.done(function(response, textStatus, jqXHR) {
+      console.log("Zapisano!");
+    });
+    request.fail(function(jqXHR, textStatus, errorThrown) {
+      console.error(
+        "The following error occurred: " +
+        textStatus, errorThrown
+      );
+    });
+    request.always(function() {
+      console.log("Wysłano");
+    });
+  });
+
 }
 
 let news_btn = document.querySelector('.news_btn');
--- news.php
+++ news.php
@@ -3,6 +3,7 @@ class News {
   private $conn;
   private $table_name = "news";
 
+  public $id;
   public $name;
   public $description;
   public $is_active;
@@ -13,8 +14,7 @@ class News {
   }
 
   function getNews() {
-
-    $query = "SELECT * FROM " .$this->table_name;
+    $query = "SELECT .$this->table_name.id, .$this->table_name.name, .$this->table_name.description, .$this->table_name.is_active, .$this->table_name.created_at, .$this->table_name.updated_at, CONCAT(registred.first_name,' ', registred.last_name) AS author FROM  .$this->table_name, registred WHERE .$this->table_name.author_id = registred.id";
     $stmt = $this->conn->prepare($query);
     $stmt->execute();
 
@@ -22,6 +22,21 @@ class News {
       $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
       return print_r(json_encode($data));
     }
+  }
+  function updateNews(){
+    $this->id=$this->id;
+    $this->name=htmlspecialchars(strip_tags($this->name));
+    $this->description=htmlspecialchars(strip_tags($this->description));
+    $this->is_active=$this->is_active;
+
+    $query = "UPDATE .$this->table_name SET name = $this->name, description = $this->description, is_active = $this->is_active WHERE id = $this->id";
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+
+    $stmt->bindParam(":id", $this->id);
+    $stmt->bindParam(":name", $this->name);
+    $stmt->bindParam(":description", $this->description);
+    $stmt->bindParam(":is_active", $this->is_active);
   }
 }
 ?>
